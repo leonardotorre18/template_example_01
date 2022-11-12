19ec40661961a3f76d495ca884ce3aba699a5003
Leonardotorre18 <leonardotorre14@gmail.com>
2022-11-11 21:32:16 -0400

    Routes files failed in deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/template_example_01/'
+  base: '/template_example_01/'
 })
